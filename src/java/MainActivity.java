63789a369bf6c60af9f5f72ef93096057e882020
Julian Liebl <jujulian1987@gmail.com>
2013-07-18 00:23:33 +0200

    MT crash fix.

--- src/java/MainActivity.java
+++ src/java/MainActivity.java
@@ -18,19 +18,31 @@ public class MainActivity extends org.haxe.nme.GameActivity {
             @Override
             public void onGlobalLayout() {
                 Rect r = new Rect();
-                //r will be populated with the coordinates of your view that area still visible.
+                //r will be populated with the coordinates of the view area still visible
                 activityRootView.getWindowVisibleDisplayFrame(r);
 
                 int screenHeight = activityRootView.getRootView().getHeight();
                 int heightDiff = screenHeight - (r.bottom - r.top);
                 boolean visible = heightDiff > screenHeight / 3;
 
-                Log.i("Softkeyboard", "Keyboard visible " + visible);
+                Log.v("Softkeyboard", "Keyboard visible " + visible);
+
+                final int keyCode;
                 if(visible){
-                    NME.onKeyChange(0x999999,true);
+                    keyCode = 0x999999;
                 }else{
-                    NME.onKeyChange(0x666666,true);
+                    keyCode = 0x666666;
                 }
+
+                new Runnable () {
+
+                    public void run () {
+
+                        NME.onKeyChange (keyCode, true);
+
+                    }
+
+                };
             }
         });
     }
