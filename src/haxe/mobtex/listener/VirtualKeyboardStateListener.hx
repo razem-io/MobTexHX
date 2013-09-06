6043bf3daa306ca52cd0ab48659d9226f6dd71d1
Julian Liebl <julian.liebl@rockabyte.com>
2013-09-06 11:55:09 +0200

    Traces will only be shown if compiled with the following flag: -Dtrace

--- src/haxe/mobtex/listener/VirtualKeyboardStateListener.hx
+++ src/haxe/mobtex/listener/VirtualKeyboardStateListener.hx
@@ -10,7 +10,9 @@ class VirtualKeyboardStateListener {
     var callbacks:Array<OnVirtualKeyboardChangeListener>;
     //if virtual keyboard state changes -> sends a signal to all registered callbacks
     private function sendCallBack(state:Bool, diffScreen:Float){
+        #if trace
         trace("Callback send at " + Date.now().getTime());
+        #end
         for(callback in callbacks){
             if(state){
                 callback.onKeyboardUp(diffScreen);
@@ -35,26 +37,36 @@ class VirtualKeyboardStateListener {
         var jni_enable_listener:Dynamic = JNI.createStaticMethod("mobtex/android/ANVirtualKeyboardListener", "enableListener", "(Lorg/haxe/nme/HaxeObject;)I");
         var returnValue:Int = jni_enable_listener(this);
         if(returnValue == 1){
+            #if trace
             trace("Enabled keyboard listener.");
+            #end
         }else{
+            #if trace
             trace("Wasn't able to set keyboard listener.");
+            #end
         }
     }
 
     public function addCallback(callback:OnVirtualKeyboardChangeListener){
         callbacks.push(callback);
+        #if trace
         trace("Current callback stack length: " + callbacks.length);
+        #end
     }
 
     public function removeCallback(callback:OnVirtualKeyboardChangeListener){
         callbacks.remove(callback);
+        #if trace
         trace("Current callback stack length: " + callbacks.length);
+        #end
     }
 
 
     //gets called by JNI when keyboard state changes
     public function onVirtualKeyboardChange(overlayScreenY:Float){
+        #if trace
         trace("Listener: virtual keyboard changed " + overlayScreenY);
+        #end
         //if screen diff is higher than 1/3 of the screen -> virtual keyboard is up
         sendCallBack(overlayScreenY > Lib.current.stage.stageWidth/0.3, overlayScreenY);
     }
